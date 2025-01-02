1924aaaca29a36f72ff205bf9944ad839ddb7b34
Adelaide Granse <grans036@umn.edu>
2025-01-02 17:39:16 -0600

    Adding Recipes

---
+++ src/main/java/com/adelsoft/recipie_services/Recipies4LifeApplication.java
@@ -0,0 +1,13 @@
+package com.adelsoft.recipie_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipies4LifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Recipies4LifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adelsoft/recipie_services/Recipies4LifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adelsoft.recipie_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipies4LifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
