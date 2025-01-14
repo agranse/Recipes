8b2bb3fc70f5588346d5137f166982e42d11dbdc
= <erik.granse@loonmagnet.net>
2025-01-13 21:57:13 -0600

    Added exception mapping

--- src/main/java/com/adelsoft/recipie_services/http/exception/NotFoundException.java
+++ src/main/java/com/adelsoft/recipie_services/http/exception/NotFoundException.java
@@ -1,7 +1,6 @@
 package com.adelsoft.recipie_services.http.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
 
